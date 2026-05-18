2a73b89a9823ccfc0512ce52dcf115f3ca96c866
Lalin Budula <118588960+Lalin-23@users.noreply.github.com>
2026-05-18 11:28:04 +0530

    Time: 3 ms (91.65%) | Memory: 19.4 MB (10.93%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        d1={}
+        d2={}
+
+        for i in range(len(s)):
+
+            c1=s[i]
+            c2=t[i]
+
+            if c1 in d1:
+                if d1[c1]!=c2:
+                    return False
+            else:
+                    d1[c1]=c2
+
+            if c2 in d2:
+                if d2[c2]!=c1:
+                    return False
+            else:
+                    d2[c2]=c1
+        return True                    
+
+
+
+               
+        
