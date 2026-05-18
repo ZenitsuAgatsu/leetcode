90fe96f2fef69dc2d889bd1ef275320c7c77a309
Lalin Budula <118588960+Lalin-23@users.noreply.github.com>
2026-05-18 12:27:03 +0530

    Time: 33 ms (6.95%) | Memory: 19.4 MB (94.27%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,3 +1,21 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return not(Counter(ransomNote)-Counter(magazine))
+        d={}
+        d2={}
+        x=ransomNote
+        y=magazine
+
+        for i in range(len(x)):
+            d[x[i]]=d.get(x[i],0)+1
+
+        for j in range(len(y)):
+            d2[y[j]]=d2.get(y[j],0)+1    
+
+        for ch in x:
+            if ch in d2:
+                if d2[ch]<d[ch]:
+                    return False   
+            else:
+                return False  
+        return True           
+        
